65050ad0424a825a124dd8163cbf4a99aab3aab8
Jewoook <jewooook@gmail.com>
2020-02-18 16:21:58 +0900

    add new project-web

---
+++ web/cmenu-web/src/components/menu/Menu.vue
@@ -0,0 +1,141 @@
+<template>
+  <v-container fluid>
+    <v-data-iterator
+      :items="items"
+      :items-per-page.sync="itemsPerPage"
+      hide-default-footer
+    >
+      <template v-slot:default="props">
+        <v-row>
+          <v-col
+            v-for="item in props.items"
+            :key="item.name"
+            cols="7"
+            sm="7"
+            md
+            lg
+          >
+            <v-card>
+              <v-card-title class="subheading font-weight-bold">{{
+                item.name
+              }}</v-card-title>
+
+              <v-divider></v-divider>
+
+              <v-list dense>
+                <v-list-item>
+                  <v-list-item-content>Calories:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.calories
+                  }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Fat:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.fat
+                  }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Carbs:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.carbs
+                  }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Protein:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.protein
+                  }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Sodium:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.sodium
+                  }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Calcium:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.calcium
+                  }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Iron:</v-list-item-content>
+                  <v-list-item-content class="align-end">{{
+                    item.iron
+                  }}</v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-iterator>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    itemsPerPage: 5,
+    items: [
+      {
+        name: "MON",
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        sodium: 87,
+        calcium: "14%",
+        iron: "1%"
+      },
+      {
+        name: "TUE",
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        sodium: 129,
+        calcium: "8%",
+        iron: "1%"
+      },
+      {
+        name: "WED",
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        sodium: 337,
+        calcium: "6%",
+        iron: "7%"
+      },
+      {
+        name: "THU",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        sodium: 413,
+        calcium: "3%",
+        iron: "8%"
+      },
+      {
+        name: "FRI",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        sodium: 413,
+        calcium: "3%",
+        iron: "8%"
+      }
+    ]
+  })
+};
+</script>
